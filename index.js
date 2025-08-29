6df314ad1e8b3181c8585761857d7458343096bf
Aniket Jangid <anikettech1460@gmail.com>
2025-08-29 15:34:05 +0530

    fixed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     try {
         res.json({ 
-            message: "Hello",
+            message: "Hellooooooooooooo",
             timestamp: new Date().toISOString(),
         });
     } catch (error) {
